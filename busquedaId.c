0b084ece8769ddb375dbf200f2037803461d64ba
Augusto Maza <augusto@slackware.slackware>
2018-05-07 22:45:57 -0300

    testeando la busqueda por ID de propietario

---
+++ busquedaId.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+
+int busquedaId (Propietarios prop[], int cant)
+
+{
+int i;
+int dato;
+char flag='F';
+
+printf("\nIngrese el ID a Buscar: ");
+scanf("%d", &dato);
+getchar();
+
+i=0;
+
+while ((flag=='F') && (prop[i].id<cant))
+{
+if(prop[i].id == dato)
+{
+    flag = 'V';
+}
+i++;
+}
+
+if(flag=='F')
+{
+
+printf("\nEl ID no existe");
+
+}
+
+else if(flag=='V')
+{
+printf("\nEl ID existe y pertenece a: %s", prop[i].nombre);
+}
+    return dato;
+}
